c25c2fe7b0855aa45dc00f221e64a9adf017b9c8
Emily Meyer <emily.christiansen@gmail.com>
2024-06-02 13:52:33 -0500

    Removed Wirebox

--- ui_tests/Test_ProofOfConcept.js
+++ ui_tests/Test_ProofOfConcept.js
@@ -2,5 +2,9 @@ Feature('Proof of Concept');
 
 Scenario('open my website', ({ I }) => {
     I.amOnPage('/');
+    I.wait(1);
     I.see("Athlete Skill Tracker");
+
+    I.amOnPage('/athletes/add.cfm');
+    I.wait(5);
 });
